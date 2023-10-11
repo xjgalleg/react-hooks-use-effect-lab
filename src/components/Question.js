52431b488940371b531b56f2ba68825f0ad35996
Josue Gallegos <gallegosjosue6@gmail.com>
2023-10-11 18:07:14 -0500

    complete use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  const [questionAnswered, setQuestionAnswered] = useState(false);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTime) => {
+        if (prevTime === 1) {
+          // If the timer reaches 1 second, call onAnswered with false
+          onAnswered(false);
+          return 10; // Reset the timer to 10 seconds
+        } else {
+          return prevTime - 1; // Decrement the time remaining
+        }
+      });
+    }, 1000);
+  
+    return () => {
+      clearTimeout(timer); // Cleanup the timer
+    };
+  }, [timeRemaining, onAnswered]);
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
+    setQuestionAnswered(true);
     onAnswered(isCorrect);
   }
 
